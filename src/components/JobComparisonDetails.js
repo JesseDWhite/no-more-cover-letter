dd5778233f9ed8d2c16aee4cac90ed47a3c419c1
Jesse White <jesse.white6@gmail.com>
2021-07-27 14:02:24 -0700

    :sparkles: list of keywords now shows as a list and does not break getScore.

--- src/components/JobComparisonDetails.js
+++ src/components/JobComparisonDetails.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
 function JobComparisonDetails(props) {
+  const test = () => {
+    const words = props.jobPostingKeyWords;
+    if (words !== undefined) {
+      return (
+        <ul>
+          {words.map(keyWord => (
+            <li key={keyWord}>{keyWord}</li>
+          ))}
+        </ul>
+      );
+    }
+  };
   return (
     <>
       <h1>Details Page</h1>
       <div className='row'>
-        <div className='col-6'>
+        <div className='col-6 card'>
           <h3>Cover Letter</h3>
           <p>{props.jobComparison.coverLetter}</p>
         </div>
-        <div className='col-6'>
+        <div className='col-6 card'>
           <h3>Job Posting</h3>
           <p>{props.jobComparison.jobPosting}</p>
         </div>
       </div>
       <p>Job Posting Key Words:</p>
-      <ul>{props.jobPostingKeyWords}</ul>
+      <p>{props.jobPostingKeyWords}</p>
+      <p>{test()}</p>
+      {/* <ul>
+        {props.jobPostingKeyWords.map(keyWord => (
+          <li>{keyWord}</li>
+        ))}
+      </ul> */}
       <p>Keywords that match: {props.yourScore}/{props.totalScore}</p>
       <p>Cover Letter Grade: {props.yourPercentage}%</p>
       <button onClick={() => props.extractKeywords(props.jobComparison.coverLetter, props.jobComparison.jobPosting)}>
